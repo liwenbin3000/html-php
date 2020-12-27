ed2efb49834a7fa7bf94be3ada99142a4622518d
linjay0609 <1927251216@qq.com>
2020-12-27 16:37:46 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,18 +1,28 @@
 <?php
 
 class dataBase{
-	
-	public function connect($localname,$username,$password,$dbname){
+	private $conn;
+	public function connect($localname,$username,$password){
 		$conn=mysqli_connect($localname,$username,$password);
+		$this->conn=$conn;
 		if($conn->connect_error){
-			echo "链接失败";
+			echo "链接失败<br/>";
 			exit;
 		}
 		else{
-			echo "链接成功";
+			echo "链接成功<br/>";
 		}
+	}
+	
+	public function close(){
+		$res=mysqli_close($this->conn);
+		if($res){echo"关闭成功<br/>";}
+		else{echo"关闭失败<br/>";}
+	}
+	
+	public function add($dbname,$statement){
 		mysqli_select_db($conn,$dbname);
-		
+		mysqli_query($conn,$statement);
 	}
 }
 
