c1c5fde99b1aa485ea179242761d01534d53d0c8
linjay0609 <1927251216@qq.com>
2020-12-27 16:13:09 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,15 +1,20 @@
 <?php
-$db=new database;
-$db->connect("localhost","root1","");
 
-class database
-{
-    function connect($servername,$username,$password){
-        $conn = new mysqli($servername, $username, $password);
-        if ($conn->connect_error) {
-            echo "连接失败";
-        }else{
-            echo "连接成功";
-        }
-    }
+class dataBase{
+	
+	public function connect($localname,$username,$password,$dbname){
+		$conn=mysqli_connect($localname,$username,$password);
+		if($conn->connect_error){
+			echo "链接失败";
+			exit;
+		}
+		else{
+			echo "链接成功";
+		}
+		mysqli_select_db($conn,$dbname);
+		
+	}
 }
+
+
+?>
