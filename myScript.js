75c825f8542d646f8844a6dfe1f47bcdc0fdb883
liangaoxiang <54211937+liangaoxiang@users.noreply.github.com>
2021-01-04 20:51:32 +0800

    ajax完善

--- myScript.js
+++ myScript.js
@@ -19,36 +19,7 @@ function ajaxRequest(t) {
 	}
 }
 
-function ajaxRequest1(t) {
-	if (window.XMLHttpRequest) {
-		xmlhttp = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	if (xmlhttp != null) {
-		xmlhttp.onreadystatechange = state_Change1;
-		form = document.forms[0];
-		String = "username" + "=" + form.username.value;
-		String += "&password=" + form.password.value;
-		xmlhttp.open("POST", 'signup-xml.php', true);
-		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.send(String);
-	} else {
-		alert("您的浏览器不支持 XMLHTTP.");
-	}
-}
-
-
 function state_Change() {
-	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		res = xmlhttp.responseXML;
-		content = res.getElementsByTagName("content")[0].innerHTML;
-		name = res.getElementsByTagName("name")[0].innerHTML;
-		document.getElementById(name).innerHTML = content;
-	}
-}
-
-function state_Change1() {
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 		res = xmlhttp.responseXML;
 		content = res.getElementsByTagName("content")[0].innerHTML;
