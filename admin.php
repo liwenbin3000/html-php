f183e45eb13c2683fe850a5a5f57b160b5355c95
liangaoxiang <54211937+liangaoxiang@users.noreply.github.com>
2021-01-07 00:34:35 +0800

    后台管理员页面

--- admin.php
+++ admin.php
@@ -1,39 +1,35 @@
-<html>
-	<head>
-		<meta charset='UTF-8'>
-		<title>新闻管理系统</title>
-	</head>
-	<body>
-		<div class='container'>
-			<div id='head'>
-				这里放管理员用户名和返回主界面的按钮和退出按钮
-				<span>管理员用户名</span>
-				<a>返回主页面</a>
-				<a>退出登录</a>
+<?php
+include "database.php";
+$hostname='localhost';
+$username='root';
+$password='';
+$dbname='class';
+
+$db = new database();
+$db->connect($hostname,$username,$password,$dbname);
+session_start();
+echo"
+		<html>
+			<head>
+				<meta charset='UTF-8'>
+				<title>新闻管理系统</title>
+				<script src="myScript.js"></script>
+			</head>
+			<body>
+			<div class='container'>
+				<div id='head'>
+					<div id='user'>
+						<span>"."{$_SESSION["username"]}"."</span>
+						<a href='quit.php'><button id='logbutt' onclick=''>退出登录</button></a>
+						<a href='homepage.php?'><button id='homebutt'>返回主页</button></a>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div id='wrapper'>
-			<div >
-				<table>
-					<tr>
-						<td>
-							<a>新闻标题1</a>
-						</td>
-						<td>
-							<input type="button" value="删除该新闻"></input>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<a>新闻标题2</a>
-						</td>
-						<td>
-							<input type="button" value="删除该新闻"></input>
-						</td>
-					</tr>
-				</table>
-				<a>添加新新闻</a>
-			</div>
-		</div>
-	</body>
-</html>
+			<div id='wrapper'>
+				<div>";
+					$db->getNewsTitle1();
+echo"	
+				</div>
+				<a herf=''>添加新新闻</a>
+			</div>";
+?>
