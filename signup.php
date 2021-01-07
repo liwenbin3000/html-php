a13eb861d26048421aff6bf5cf24158380250e71
linjay0609 <1927251216@qq.com>
2021-01-07 14:20:29 +0800

    修改了一定的bug

--- signup.php
+++ signup.php
@@ -8,7 +8,7 @@ $dbname='class';
 $account=$_POST["username"];
 $password1=$_POST["password"];
 $date=new Datetime();
-$time=$date->format("Y-m-d-H-i-s");
+$time=$date->format("Y-m-d H:i:s");
 $db = new database();
 $db->connect($hostname,$username,$password,$dbname);
 $db->addPeople($account,$password1,0,$time);
