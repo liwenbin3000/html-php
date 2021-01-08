75fbce2f4875f281b3d1563f4a825666cad96bca
linjay0609 <1927251216@qq.com>
2021-01-08 23:17:48 +0800

    Update changenews.php

--- changenews.php
+++ changenews.php
@@ -30,18 +30,28 @@ if($type==1){
 if($type==2){
 	echo "
 	<html>
+	<head>
 	    <link rel='stylesheet' type='text/css' href='changestyle.css?v=2'>
 	</head>
 	<body>
+	<script type='text/javascript'>
+	function myFunction(){
+		if (document.getElementById('newsTitle').value == '' || document.getElementById('Content').value == ''){
+			alert('内容不能为空');
+			return false;
+		}
+		else{return true;}
+	}
+	</script>
 	<div class='container'>
 	<div id='news'>
-	<form id='newsForm' method='post'><tr>
+	<form id='newsForm' method='post' onsubmit='return myFunction()'><tr>
 			<h3>标题</h3>
 	    <td id='newsTitle' ><input name='newsTitle' type=textarea id='newsTitle'></input></td></tr>
 		<h3>内容</h3>
-		<td id='newsContent'><textarea id='Content'></textarea><br>
-		<input type=submit value='增加' name='add' id='submit'></input></td></tr>
-		
+		<td id='newsContent'><textarea id='Content' name='Content'></textarea><br>
+		<input type=submit value='增加' name='add' id='submit' ></input></td></tr>
+		<a href =admin.php><input type=button value='返回管理员界面'></input></a>
 	    </tr>
 	</form>
 	</div>
@@ -51,15 +61,16 @@ if($type==2){
 		";
 	if(isset($_POST['add'])){
 	$title=$_POST['newsTitle'];
-	$content=$_POST['newsContent'];
+	$content=$_POST['Content'];
 	$date=new Datetime();
 	$time=$date->format("Y-m-d");
 	while($title!='' and $content!=''){
 		$db->addNews($title,$content,$time);
 		$title='';
 		$content='';
+		header("Location:admin.php");
 		}
-    header("Location:admin.php");
+
 	}   
 }
 
